c4815a36f3c79e5b5cf9e9194df5e48812255c9e
nickromaninsky <nickromo28@gmail.com>
2023-02-23 18:12:01 -0500

    Finished UI Redesign. 🎮

--- cubeSolver.py
+++ cubeSolver.py
@@ -21,8 +21,9 @@ FPS = 30
 
 pygame.display.set_caption("Rubik's Cube Solver")
 
-font = pygame.font.Font(None, 36)
-font2 = pygame.font.Font(None, 26)
+font = pygame.font.SysFont("Arial", 25)
+font2 = pygame.font.SysFont("Arial", 15)
+
 
 info = pygame.display.Info()
 screen_width = info.current_w
@@ -33,21 +34,24 @@ cube_image = pygame.image.load('cube_image.jpeg')
 cube_image = pygame.transform.scale(cube_image, (250, 250))  
 
 order_image = pygame.image.load('order.png')           
-order_image = pygame.transform.scale(order_image, (400, 400))  
+order_image = pygame.transform.scale(order_image, (600, 400))
+
+example_image = pygame.image.load('example.png')           
+example_image = pygame.transform.scale(example_image, (250, 250))  
 
 # create the input boxes
-input_box1 = pygame.Rect(100, 125, 300, 50)
+input_box1 = pygame.Rect(25, 125, 300, 50)
 text = [''] * 6
 
-input_box2 = pygame.Rect(100, 225, 300, 50)
+input_box2 = pygame.Rect(25, 225, 300, 50)
 
-input_box3 = pygame.Rect(100, 325, 300, 50)
+input_box3 = pygame.Rect(25, 325, 300, 50)
 
-input_box4 = pygame.Rect(100, 425, 300, 50)
+input_box4 = pygame.Rect(25, 425, 300, 50)
 
-input_box5 = pygame.Rect(100, 525, 300, 50)
+input_box5 = pygame.Rect(25, 525, 300, 50)
 
-input_box6 = pygame.Rect(100, 625, 300, 50)
+input_box6 = pygame.Rect(25, 625, 300, 50)
 
 colors = [pygame.Color('black')] * 6
 active = [False] * 6
@@ -56,13 +60,13 @@ x = 2
 y = 1
 
 # create the quit button
-quit_button = pygame.Rect(1300, 25, 100, 50)
+quit_button = pygame.Rect(1300, 15, 100, 50)
 
 # create the again button
-again_button = pygame.Rect(1100, 25, 175, 50)
+again_button = pygame.Rect(1100, 15, 175, 50)
 
 # create the solve button
-solve_button = pygame.Rect(1200, 475, 125, 50)
+solve_button = pygame.Rect(1250, 150, 125, 50)
 
 solve_clicked = False
 
@@ -108,29 +112,29 @@ def draw_window():
     clock.tick(FPS)
     screen.fill(white)
 
-    text1_surface = font.render("Enter Yellow Side (0 - 8): ", True, black)
-    screen.blit(text1_surface, (100, 100))
+    text1_surface = font.render("Enter Yellow Side (0 - 8)", True, black)
+    screen.blit(text1_surface, (25, 100))
 
-    text2_surface = font.render("Enter Blue Side (9 - 17): ", True, black)
-    screen.blit(text2_surface, (100, 200))
+    text2_surface = font.render("Enter Blue Side (9 - 17)", True, black)
+    screen.blit(text2_surface, (25, 200))
 
-    text3_surface = font.render("Enter Red Side (18 - 26): ", True, black)
-    screen.blit(text3_surface, (100, 300))
+    text3_surface = font.render("Enter Red Side (18 - 26)", True, black)
+    screen.blit(text3_surface, (25, 300))
 
-    text4_surface = font.render("Enter Green Side (27 - 35): ", True, black)
-    screen.blit(text4_surface, (100, 400))
+    text4_surface = font.render("Enter Green Side (27 - 35)", True, black)
+    screen.blit(text4_surface, (25, 400))
 
-    text5_surface = font.render("Enter Orange Side (36 - 44): ", True, black)
-    screen.blit(text5_surface, (100, 500))
+    text5_surface = font.render("Enter Orange Side (36 - 44)", True, black)
+    screen.blit(text5_surface, (25, 500))
 
-    text6_surface = font.render("Enter White Side (45 - 53): ", True, black)
-    screen.blit(text6_surface, (100, 600))
+    text6_surface = font.render("Enter White Side (45 - 53)", True, black)
+    screen.blit(text6_surface, (25, 600))
 
-    solution_surface = font2.render("Step One: Position cube like this, with yellow center piece on top and red center", True, black)
-    screen.blit(solution_surface, (415, 50)) 
+    solution_surface = font2.render("Step One: Position cube like this, with yellow center piece on top and red center piece facing you. The center piece", True, black)
+    screen.blit(solution_surface, (340, 50)) 
 
-    solution_surface = font2.render("piece facing you. The center piece of each side never moves and tells you what color that side will be when the cube is solved.", True, black)
-    screen.blit(solution_surface, (415, 75)) 
+    solution_surface = font2.render("of each side never moves and tells you what color that side will be when the cube is solved.", True, black)
+    screen.blit(solution_surface, (340, 75)) 
 
     # render the input boxes
     pygame.draw.rect(screen, colors[0], input_box1, 2)
@@ -180,28 +184,62 @@ def draw_window():
         solution = utils.solve(new_cube5, "Kociemba")
         solution_text = str(solution)  # convert to string
         answer_surface = font.render(solution_text, True, black)
-        screen.blit(answer_surface, (700, 525))
-        text9_surface = font.render("Thank you for using my Rubik's Cube Solver!", True, black)
-        screen.blit(text9_surface, (700, 675))
-        text10_surface = font.render("Click the run again button to try again,", True, black)
-        screen.blit(text10_surface, (700, 700))
-        text11_surface = font.render("or the quit button to quit the program.", True, black)
-        screen.blit(text11_surface, (700, 725))
+        screen.blit(answer_surface, (25, 700))
+        #text9_surface = font.render("Thank you for using my Rubik's Cube Solver!", True, black)
+        #screen.blit(text9_surface, (700, 675))
+        #text10_surface = font.render("Click the run again button to try again,", True, black)
+        #screen.blit(text10_surface, (700, 700))
+        #text11_surface = font.render("or the quit button to quit the program.", True, black)
+        #screen.blit(text11_surface, (700, 725))
     
 
 
-    #screen.blit(cube_image, (415, 525))
+    screen.blit(cube_image, (340, 100))
 
-    #screen.blit(order_image, (415, 70))
+    screen.blit(order_image, (950, 400))
 
-    #text8_surface = font2.render("You MUST hold the cube like this the whole time. Look at the other sides from this angle.", True, black)
-    #screen.blit(text8_surface, (415, 500))
+    text8_surface = font2.render("Enter colors of scramble into fields at left in the numeral order set out below. Type in the first lettor of each color.", True, black)
+    screen.blit(text8_surface, (340, 375))
+
+
+    screen.blit(example_image, (340, 400))
+
+    text8_surface = font2.render("Example: gwryygrgo", True, black)
+    screen.blit(text8_surface, (375, 600))
     
-    #text100_surface = font2.render("Position cube like this, with yellow center peice on top and red center peice facing you.", True, black)
-    #screen.blit(text100_surface, (415, 475))
+    text100_surface = font2.render("Step Two: Click solve and execute moves! Be sure to", True, black)
+    screen.blit(text100_surface, (1100, 100))
     
-    #text400_surface = font2.render("Type in the colors in this order. Type in the first letter of the color for each facelet.", True, black)
-    #screen.blit(text400_surface, (400, 50))
+    text400_surface = font2.render("maintain position of cube with yellow center on top and", True, black)
+    screen.blit(text400_surface, (1100, 125))
+    
+    text401_surface = font2.render("red center facing you.", True, black)
+    screen.blit(text401_surface, (1100, 150))
+
+    text401_surface = font2.render("U = Upper Layer (Horizontal)", True, black)
+    screen.blit(text401_surface, (1100, 200))
+
+    text401_surface = font2.render("D = Lowest Layer (Horizontal)", True, black)
+    screen.blit(text401_surface, (1100, 225))
+
+    text401_surface = font2.render("R = Right Layer (Vertical)", True, black)
+    screen.blit(text401_surface, (1100, 250))
+
+    text401_surface = font2.render("L = Left Layer (Vertical)", True, black)
+    screen.blit(text401_surface, (1100, 275))
+
+    text401_surface = font2.render("F = Front Layer (Parallel To You)", True, black)
+    screen.blit(text401_surface, (1100, 300))
+
+    text401_surface = font2.render("B = Back Layer (Parallel To You)", True, black)
+    screen.blit(text401_surface, (1100, 325))
+
+    text401_surface = font2.render("2 = Do Move Twice", True, black)
+    screen.blit(text401_surface, (1100, 350))
+
+    text401_surface = font2.render("' = Counter Clockwise", True, black)
+    screen.blit(text401_surface, (1100, 375))
+    
     pygame.display.update()
 
 def main():
